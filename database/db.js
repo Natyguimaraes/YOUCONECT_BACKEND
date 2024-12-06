be3c37d7c6d7cd1d05f5e9482aa38bcba64baffb
Natyguimaraes <santosnath433@gmail.com>
2024-12-06 16:37:28 -0300

    firts

--- database/db.js
+++ database/db.js
@@ -1,20 +1,23 @@
-import mysql2 from 'mysql2';
+import mysql from 'mysql2';
 
-const connection = mysql2.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123456789',
-    database: 'youconect_project'
+// Cria a conexão com o banco de dados
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,       // Acessa a variável de ambiente DB_HOST
+    user: process.env.DB_USER,       // Acessa a variável de ambiente DB_USER
+    password: process.env.DB_PASSWORD, // Acessa a variável de ambiente DB_PASSWORD
+    database: process.env.DB_NAME,    // Acessa a variável de ambiente DB_NAME
+    port: process.env.DB_PORT || 3306 // Acessa a variável de ambiente DB_PORT (padrão 3306)
 });
 
+// Verifica se a conexão foi bem-sucedida
 connection.connect((err) => {
     if (err) {
-        console.error('Erro ao realizar conexão com o banco de dados', err);
-        throw err;
+        console.error('Erro ao conectar no banco de dados: ', err.stack);
+        return;
     }
-
-    console.log('Conexão com o banco de dados estabelecida com sucesso!');
+    console.log('Conectado ao banco de dados como id ' + connection.threadId);
 });
 
 export default connection;
+
 
